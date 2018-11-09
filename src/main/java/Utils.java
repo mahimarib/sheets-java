36849c048c90f5b419554009930cea074b0d7e84
Mahim <mahim216@gmail.com>
2018-11-08 21:52:27 -0500

    renamed class

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -5,17 +5,23 @@ import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInsta
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
 
-public class SheetsQuickstart {
+public class Utils {
     public static final String APPLICATION_NAME
             = "Google Sheets API Java Quickstart";
     public static final JsonFactory JSON_FACTORY = JacksonFactory
@@ -27,7 +33,7 @@ public class SheetsQuickstart {
      * If modifying these scopes, delete your previously saved tokens/ folder.
      */
     public static final List<String> SCOPES = Collections.singletonList(
-            SheetsScopes.SPREADSHEETS_READONLY);
+            SheetsScopes.SPREADSHEETS);
     public static final String CREDENTIALS_FILE_PATH = "credentials.json";
 
     /**
@@ -37,12 +43,13 @@ public class SheetsQuickstart {
      * @return An authorized Credential object.
      * @throws IOException If the credentials.json file cannot be found.
      */
-    public static Credential getCredentials(
+    private static Credential getCredentials(
             final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Load client secrets.
         File credentials = new File(CREDENTIALS_FILE_PATH);
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
-                JSON_FACTORY, new InputStreamReader(new FileInputStream(credentials)));
+                JSON_FACTORY,
+                new InputStreamReader(new FileInputStream(credentials)));
 
         // Build flow and trigger user authorization request.
         GoogleAuthorizationCodeFlow flow
@@ -56,5 +63,16 @@ public class SheetsQuickstart {
                 .setPort(8888).build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize(
                 "user");
+    }
+
+    public static Sheets getSheets()
+    throws GeneralSecurityException, IOException {
+        final NetHttpTransport transport =
+                GoogleNetHttpTransport.newTrustedTransport();
+        return new Sheets.Builder(
+                transport,
+                Utils.JSON_FACTORY,
+                Utils.getCredentials(transport)).setApplicationName(
+                Utils.APPLICATION_NAME).build();
     }
 }
