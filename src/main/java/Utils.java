53a4a7fbe58a47f37287ace7a7585adfe15e6cad
Mahim <mahim216@gmail.com>
2018-11-09 03:17:14 -0500

    changed import

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -56,7 +56,7 @@ public class Utils {
                 = new GoogleAuthorizationCodeFlow.Builder(
                 HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
                 .setDataStoreFactory(new FileDataStoreFactory(
-                        new java.io.File(TOKENS_DIRECTORY_PATH)))
+                        new File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
         LocalServerReceiver receiver = new LocalServerReceiver.Builder()
