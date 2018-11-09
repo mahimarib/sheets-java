1f23c10c2e4bc1deb57601b43dfa5cd1617170e5
Mahim <mahim216@gmail.com>
2018-11-08 21:52:41 -0500

    testing writing in sheets

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,48 +1,32 @@
 package main.java;
 
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.model.ValueRange;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 import java.util.List;
 
-import static main.java.SheetsQuickstart.APPLICATION_NAME;
-import static main.java.SheetsQuickstart.JSON_FACTORY;
-import static main.java.SheetsQuickstart.getCredentials;
-
 public class Test {
-    /**
-     * Prints the names and majors of students in a sample spreadsheet:
-     * https://docs.google
-     * .com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-     */
+
     public static void main(String... args)
     throws IOException, GeneralSecurityException {
-        // Build a new authorized API client service.
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport
-                .newTrustedTransport();
-        final String spreadsheetId
-                = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
-        final String range = "Class Data!A2:E";
-        Sheets service = new Sheets.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-        ValueRange response = service.spreadsheets().values()
-                                     .get(spreadsheetId, range)
-                                     .execute();
-        List<List<Object>> values = response.getValues();
-        if (values == null || values.isEmpty()) {
-            System.out.println("No data found.");
-        } else {
-            System.out.println("Name, Major");
-            for (List row : values) {
-                // Print columns A and E, which correspond to indices 0 and 4.
-                System.out.printf("%s, %s\n", row.get(0), row.get(4));
+        Sheets service = Utils.getSheets();
+        String ssID = "1vnnirg2W_QnRa8Se_yxe1ytj3lHRXkR58LpNCfZcXg8";
+        String range = "Sheet1!A1:C";
+        List<List<Object>> writeData = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            List<Object> row = new ArrayList<>();
+            for (int j = 0; j < 3; j++) {
+                row.add(Math.round(Math.random() * 10));
             }
+            writeData.add(row);
         }
+        ValueRange vr = new ValueRange().setValues(writeData).setMajorDimension("ROWS");
+        service.spreadsheets().values()
+               .update(ssID, range, vr)
+               .setValueInputOption("RAW")
+               .execute();
     }
 }
